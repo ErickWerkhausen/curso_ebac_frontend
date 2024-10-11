c7fbcd670b5f6aafb218c1c587f750c1b15b73ab
erick Gabriel Werkhausen Klein <142852118+ErickWerkhausen@users.noreply.github.com>
2024-10-10 21:16:09 -0300

    primeiro commit

---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicacao(numero1: number, numero2: number) {
+    console.log(numero1 * numero2);
+}
+
+multiplicacao(24, 54);
+
+function dizOla(nome: string) {
+    console.log(`ola ${nome}`);
+    
+}
+
+dizOla('lucas');
